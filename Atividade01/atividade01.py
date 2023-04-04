9207c4079f244110772da369eaecf9fec6562d24
augustoozin11 <127156500+augustoozin11@users.noreply.github.com>
2023-04-03 21:19:56 -0300

    Add files via upload

---
+++ Atividade01/atividade01.py
@@ -0,0 +1,14 @@
+nome = "Jose"
+sobrenome = "Augusto"
+idade = 19
+altura = 1.86
+peso = 71
+maior_de_idade = idade >= 18
+
+# Exibir informações da pessoa
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade, "anos")
+print("Altura:", altura, "m")
+print("Peso:", peso, "kg")
+print("Maior de idade?", "Sim" if maior_de_idade else "Não")
